fb5e04f20f2356a415069fa8f38fa35477d4dfca
Scode <993739033>
2019-02-25 15:02:19 +0800

    Initial commit

---
+++ app/src/main/java/com/bbny/qifengwlw/rootdemo/MainActivity.java
@@ -0,0 +1,55 @@
+package com.bbny.qifengwlw.rootdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //当前应用的代码执行目录
+        RootUtils.upgradeRootPermission(getPackageCodePath());
+//        RootUtils.execRootCmd("cd /root/data");
+//        RootUtils.execRootCmd("ls -a");
+//        RootUtils.execRootCmd("ls -al");
+
+//        try {
+//            Runtime.getRuntime().exec(new String[]{"cd /test && ls -l"});
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+//        RootUtils.execRootCmd("cd /test && ls -a");//这里必须连续使用
+//        RootUtils.execRootCmd("cd /test && find  -mtime -1");//查找今天内有改动的文件
+//        RootUtils.execRootCmd("find /test/ -newer /test/rbdbd.txt ");//查找文件夹中比文件还要新的文件
+//         RootUtils.execRootCmd("find /test/ -name rr.txt");//查找文件名相匹配的文件
+//        RootUtils.execRootCmd("find /test/ -size +10k");//才找文件size大于 12KB的文件，注意c表示byte
+//        RootUtils.execRootCmd("cp  -a /test/rr.txt /test/rr11.txt");//复制文件1成文件2
+//        RootUtils.execRootCmd("mv -f /test/rr.txt  ./");//强制移动rr.txt 文件到文件夹路径下
+//        RootUtils.execRootCmd("rm -f /rr.txt");//强制删除文件  删除文件夹需使用递归删除
+//        RootUtils.execRootCmd("ps  aux");//获取进程信息
+//        RootUtils.execRootCmd("cat /test/rr11.txt");//获取文件中的信息、
+
+//          chmod [-R] xyz 文件或目录
+//         -R：进行递归的持续更改，即连同子目录下的所有文件都会更改
+//         同时，chmod还可以使用u（user）、g（group）、o（other）、a（all）和+（加入）、-（删除）、=（设置）跟rwx搭配来对文件的权限进行更改
+
+//        RootUtils.execRootCmd("chmod u-x /test/rr11.txt");//设置文件权限
+//        RootUtils.execRootCmd("touch /test/rr22.txt");//以vim方式进行打开或创建 测试不出来 touch 可以创建一个新文件
+        RootUtils.execRootCmd("mkdir -p /test/A/B/c");//以mkdir 创建文件夹 使用-p 递归方式 创建文件夹
+//        #追加文字到文件
+//        cat >>/tmp/oldboy.txt << EOF
+//        世界，你好！
+//        EOF
+        RootUtils.execRootCmd("cat >>/test/rr22.txt << EOF 你好linux 编程 EOF");//追加文件
+//        RootUtils.execRootCmd("ls -l");
+
+    }
+
+
+}
---
+++ app/src/main/java/com/bbny/qifengwlw/rootdemo/RootUtils.java
@@ -0,0 +1,147 @@
+package com.bbny.qifengwlw.rootdemo;
+
+/**
+ * Created by ZWX on 2019/2/25.
+ */
+
+import android.util.Log;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+/**
+ * Android运行linux命令
+ */
+public class RootUtils {
+    private static final String TAG = "RootCmd";
+    private static boolean mHaveRoot = false;
+
+    /**
+     * 应用程序运行命令获取 Root权限，设备必须已破解(获得ROOT权限)
+     *
+     * @return 应用程序是/否获取Root权限
+     */
+    public static boolean upgradeRootPermission(String pkgCodePath) {
+        Process process = null;
+        DataOutputStream os = null;
+        try {
+            String cmd = "chmod 777 " + pkgCodePath;
+            process = Runtime.getRuntime().exec("su"); //切换到root帐号
+            os = new DataOutputStream(process.getOutputStream());
+            os.writeBytes(cmd + "\n");
+            os.writeBytes("exit\n");
+            os.flush();
+            process.waitFor();
+        } catch (Exception e) {
+            return false;
+        } finally {
+            try {
+                if (os != null) {
+                    os.close();
+                }
+                process.destroy();
+            } catch (Exception e) {
+            }
+        }
+        return true;
+    }
+
+
+
+    /**
+     * 判断机器Android是否已经root，即是否获取root权限
+     */
+    public static boolean haveRoot() {
+        if (!mHaveRoot) {
+            int ret = execRootCmdSilent("echo test"); // 通过执行测试命令来检测
+            if (ret != -1) {
+                Log.i(TAG, "have root!");
+                mHaveRoot = true;
+            } else {
+                Log.i(TAG, "not root!");
+            }
+        } else {
+            Log.i(TAG, "mHaveRoot = true, have root!");
+        }
+        return mHaveRoot;
+    }
+
+    /**
+     * 执行命令并且输出结果
+     */
+    public static String execRootCmd(String cmd) {
+        String result = "";
+        DataOutputStream dos = null;
+        DataInputStream dis = null;
+
+        try {
+            Process p = Runtime.getRuntime().exec("su");// 经过Root处理的android系统即有su命令
+            dos = new DataOutputStream(p.getOutputStream());
+            dis = new DataInputStream(p.getInputStream());
+
+            Log.i(TAG, cmd);
+            dos.writeBytes(cmd + "\n");
+            dos.flush();
+            dos.writeBytes("exit\n");
+            dos.flush();
+            String line = null;
+            while ((line = dis.readLine()) != null) {
+                Log.d("result", line);
+                result += line;
+            }
+            p.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (dos != null) {
+                try {
+                    dos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (dis != null) {
+                try {
+                    dis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 执行命令但不关注结果输出
+     */
+    public static int execRootCmdSilent(String cmd) {
+        int result = -1;
+        DataOutputStream dos = null;
+
+        try {
+            Process p = Runtime.getRuntime().exec("su");
+            dos = new DataOutputStream(p.getOutputStream());
+
+            Log.i(TAG, cmd);
+            dos.writeBytes(cmd + "\n");
+            dos.flush();
+            dos.writeBytes("exit\n");
+            dos.flush();
+            p.waitFor();
+            result = p.exitValue();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (dos != null) {
+                try {
+                    dos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
+    }
+
+}
